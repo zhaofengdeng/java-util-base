e6782ffda06266da27dbd1cc513392e8a2c56bfd
zhaofengdeng <zhao_fengdeng@khktsoft.com>
2019-06-27 16:24:59 +0800

    日期工具类扩展

--- src/main/java/com/util/base/DateUtil.java
+++ src/main/java/com/util/base/DateUtil.java
@@ -2,6 +2,7 @@ package com.util.base;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -58,6 +59,42 @@ public class DateUtil {
 		}
 
 		return null;
+	}
+	/**
+	 * 增加天数，负数为减
+	 * @param date
+	 * @param day
+	 * @return
+	 */
+	public static Date addDay(Date date,Integer day) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date); 
+		calendar.add(Calendar.DATE, day);
+		return calendar.getTime();
+	}
+	/**
+	 * 增加月份，负数为减
+	 * @param date
+	 * @param month
+	 * @return
+	 */
+	public static Date addMonth(Date date,Integer month) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date); 
+		calendar.add(Calendar.MONTH, month);
+		return calendar.getTime();
+	}
+	/**
+	 * 增加年，负数为减
+	 * @param date
+	 * @param year
+	 * @return
+	 */
+	public static Date addYear(Date date,Integer year) {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(date); 
+		calendar.add(Calendar.YEAR, year);
+		return calendar.getTime();
 	}
 
 }
