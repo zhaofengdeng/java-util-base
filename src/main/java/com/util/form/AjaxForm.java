0d6977cb86de3fcf13859178db9efadf3339f9e9
zhaofengdeng <zhao_fengdeng@khktsoft.com>
2019-09-26 18:19:22 +0800

    版本升级至0.0.10增加AjaxForm类

---
+++ src/main/java/com/util/form/AjaxForm.java
@@ -0,0 +1,37 @@
+package com.util.form;
+
+
+public class AjaxForm {
+	public Integer code;
+	public Boolean success = false;
+	public Object data;
+
+	public AjaxForm setSuccess(Object data) {
+		this.success = true;
+		this.data = data;
+		return this;
+	}
+
+	public AjaxForm setError(Integer errorCode, String errorMsg) {
+		this.success = false;
+		this.data = errorMsg;
+		return this;
+	}
+	public AjaxForm setError(String errorMsg) {
+		this.success = false;
+		this.data = errorMsg;
+		return this;
+	}
+
+	public Integer getCode() {
+		return code;
+	}
+
+	public Boolean getSuccess() {
+		return success;
+	}
+
+	public Object getData() {
+		return data;
+	}
+}
